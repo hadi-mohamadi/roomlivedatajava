b1a86287d0726147d3d834db60b38b3e0fff026d
hadi-mohamadi <work.hadimohammadi@gmail.com>
2018-10-15 17:53:42 +0330

    add test

---
+++ app/src/androidTest/java/com/example/root/roomlivedatajava/view/MainActivityTest.java
@@ -0,0 +1,97 @@
+package com.example.root.roomlivedatajava.view;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import com.example.root.roomlivedatajava.R;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.allOf;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void showElementsTest() {
+
+
+        ViewInteraction viewGroup = onView(
+                allOf(childAtPosition(
+                        allOf(withId(android.R.id.content),
+                                childAtPosition(
+                                        withId(R.id.decor_content_parent),
+                                        1)),
+                        0),
+                        isDisplayed()));
+        viewGroup.check(doesNotExist());
+
+        ViewInteraction imageButton = onView(
+                allOf(withId(R.id.add),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                1),
+                        isDisplayed()));
+        imageButton.check(matches(isDisplayed()));
+
+    }
+
+
+
+    @Test
+    public void fabButtonClickTest() {
+
+        ViewInteraction floatingActionButton = onView(
+                allOf(withId(R.id.add),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                1),
+                        isDisplayed()));
+        floatingActionButton.perform(click());
+
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
